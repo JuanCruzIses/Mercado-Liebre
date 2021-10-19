b2d3883a0f8f9eb4472aa005edba465edd099c97
JuanCruzIses <juancruzises@gmail.com>
2021-10-18 21:47:47 -0300

    Subiendo el mercado liebre a github

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+const PUERTO = 3000;
+app.use(express.static('./public'));
+
+
+app.get('/', function(req, res) { res.sendFile(path.join(__dirname, './views/home.html'))} )
+
+app.get('/register', function(req, res) { res.sendFile(path.join(__dirname, './views/register.html'))} )
+
+app.get('/login', function(req, res) { res.sendFile(path.join(__dirname, './views/login.html'))} )
+
+
+app.listen(PUERTO, () => console.log('Servidor corriendo'))
